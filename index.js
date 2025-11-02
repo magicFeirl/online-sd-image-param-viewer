a837d8ebd9d8e595570f9d8e514b315510e6ef2f
magicFeirl <2100709458@qq.com>
2025-11-02 15:06:15 +0800

    feat: add .webp file format；add clipboard api

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function handleAddFiles(files) {
     }
 
     ExifReader.load(file).then((tags) => {
+      debugger
       appendResult(file, tags);
     });
   }
@@ -92,20 +93,26 @@ const paramsToHTMLStr = (params) => {
   return result
     .map(
       (s) =>
-        `<span class="tag ${
-          highlight_keys.some((k) => s.includes(k)) ? "highlight" : ""
+        `<span class="tag ${highlight_keys.some((k) => s.includes(k)) ? "highlight" : ""
         }">${s.replace(/\\n/g, "<br/>")}</span>`
     )
     .join("\n");
 };
 
 const parseParams = (tags) => {
-  const p = tags["parameters"];
-  if (!p || !p["value"].trim()) {
+  const p = tags["parameters"] || tags["UserComment"];
+  let value = p.value
+
+  if (Array.isArray(value)) {
+    value = value[0]
+  }
+
+  if (!value || !value.trim()) {
     alert("Unknown file/No metadata found");
     return;
   }
-  const v = p["value"];
+
+  const v = value;
   const ng_idx = v.indexOf("Negative prompt:");
   const param_idx = v.indexOf("Steps:");
 
@@ -133,17 +140,16 @@ const createResultItem = (dataURL, params) => {
       </div>
       <div class="info">
         ${Object.keys(params)
-          .map(
-            (title) => `<div>
+      .map(
+        (title) => `<div>
           <div class="title">${title}<span class="copy" data-title="${title}"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-copy "><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z"></path><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1"></path></svg></span></div>
-          <div class="content">${
-            title === "parameters"
-              ? paramsToHTMLStr(params[title])
-              : params[title]
+          <div class="content">${title === "parameters"
+            ? paramsToHTMLStr(params[title])
+            : params[title]
           }</div>
         </div>`
-          )
-          .join("\n")}
+      )
+      .join("\n")}
       </div>
     `;
 
@@ -176,3 +182,27 @@ function appendResult(file, tags) {
     node.scrollIntoView();
   };
 }
+
+
+document.addEventListener('paste', (event) => {
+  const items = (event.clipboardData || window.clipboardData).items;
+  if (!items) {
+    return alert('Can\' access clipboard')
+  }
+
+
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+
+    if (item.kind === 'file' && item.type.startsWith('image/')) {
+
+      event.preventDefault();
+
+      const imageFile = item.getAsFile();
+
+      if (imageFile) {
+        handleAddFiles([imageFile]);
+      }
+    }
+  }
+});
